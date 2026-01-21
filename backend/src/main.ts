bbdd2c05c335cacde18d5e7650858adb439fc491
Felix Wachira <wachirafeliki@gmail.com>
2026-01-21 16:47:34 +0300

    update in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
 
-  await app.listen(3001);
+ const port = process.env.PORT || 3001
+await app.listen(port);
 }
 bootstrap();
